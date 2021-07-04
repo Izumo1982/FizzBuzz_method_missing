2ee0a9ae1a372f134064fd0066370d68a623e356
Izumo1982 <84167062+Izumo1982@users.noreply.github.com>
2021-07-04 23:07:49 +0900

    Update fizzbuzz_method_missing.rb

--- fizzbuzz_method_missing.rb
+++ fizzbuzz_method_missing.rb
@@ -1,11 +1,11 @@
 ﻿class BasicObject
-	def method_missing(m_name, *args)
-		return exit if m_name.to_s != "fizz_buzz"
-  	str = 'Fizz'      if args[0] % 3 == 0
-  	str = 'Buzz'      if args[0] % 5 == 0
-  	str = 'Fizz Buzz' if args[0] % 15 == 0
-  	str ||= args[0].to_s
-	end
+  def method_missing(m_name, *args)
+    return exit if m_name.to_s != "fizz_buzz"
+    str = 'Fizz'      if args[0] % 3 == 0
+    str = 'Buzz'      if args[0] % 5 == 0
+    str = 'Fizz Buzz' if args[0] % 15 == 0
+    str ||= args[0].to_s
+  end
 end
 
 for i in 1..100 do
