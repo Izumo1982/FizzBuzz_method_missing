ed2e58a1a52b0f5eb6d92a17980e41a7c79349ad
Izumo1982 <84167062+Izumo1982@users.noreply.github.com>
2021-07-04 23:06:33 +0900

    Add files via upload

---
+++ fizzbuzz_method_missing.rb
@@ -0,0 +1,13 @@
+﻿class BasicObject
+	def method_missing(m_name, *args)
+		return exit if m_name.to_s != "fizz_buzz"
+  	str = 'Fizz'      if args[0] % 3 == 0
+  	str = 'Buzz'      if args[0] % 5 == 0
+  	str = 'Fizz Buzz' if args[0] % 15 == 0
+  	str ||= args[0].to_s
+	end
+end
+
+for i in 1..100 do
+  p fizz_buzz(i)
+end
